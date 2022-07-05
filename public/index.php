19c15f5ad52c1461ef598bf2ba435fabf5b10510
Pavel <klimenko@giperlink.by>
2022-07-05 13:59:56 +0300

    composer updated

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
-
+//TODO версию php 8.1 поставить
+//phpinfo();
+//exit();
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
